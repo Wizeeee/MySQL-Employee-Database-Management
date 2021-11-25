5938479260d72fa199c3dc37518effb7ed5b0dbb
wizeeee <wizeman22@outlook.com>
2021-11-24 21:17:02 -0500

    employee query

--- app.js
+++ app.js
@@ -118,3 +118,96 @@ const viewAllEmployees = () => {
     manageEmployees();
   });
 };
+
+//                --View All Departments Function (QUERY)--
+// -----------------------------------------------------------------------
+
+const viewAllDepartments = () => {
+  //save mysql query as sql to use in actual query below
+  const sql = `SELECT department.id AS ID,
+                      department.dept_name AS Department
+               FROM department`;
+  //database query (sql query, callback function)
+  db.query(sql, (err, res) => {
+    //catch errors if any exist
+    if (err) {
+      console.log(err);
+    }
+    //construct table in node.js from query
+    console.table(res);
+    //call initializing function to return to inquirer prompt
+    manageEmployees();
+  });
+};
+
+//                --View All Roles Function (QUERY)--
+// -----------------------------------------------------------------------
+
+const viewAllRoles = () => {
+  //save mysql query as sql to use in actual query below
+  const sql = `SELECT role.id AS ID,
+                      role.title AS Role,
+                      department.dept_name AS Department,
+                      role.salary AS Salary
+               FROM role
+                      LEFT JOIN Department ON role.department_id = department.id`;
+  //database query (sql query, callback function)
+  db.query(sql, (err, res) => {
+    //catch errors if any exist
+    if (err) {
+      console.log(err);
+    }
+    //construct table in node.js from query
+    console.table(res);
+    //call initializing function to return to inquirer prompt
+    manageEmployees();
+  });
+};
+
+//                --View Employees by Department (QUERY)--
+// -----------------------------------------------------------------------
+
+const viewAllByDepartment = () => {
+  //mysql query : Select All from department table
+  db.query(`SELECT * FROM department`, (err, res) => {
+    if (err) {
+      console.log(err);
+    }
+    //create new array with key and value for each department
+    const chooseDept = res.map(({ id, dept_name }) => ({
+      name: dept_name,
+      value: id,
+    }));
+
+    //prompt user with a list of departments to choose from
+    inquirer
+      .prompt({
+        type: "list",
+        name: "department",
+        message: "Which department would you like to see?",
+        //call chooseDept array for choices
+        choices: chooseDept,
+      })
+      .then((choice) => {
+        //deconstruct "name:" and save as choice parameter
+        const { department } = choice;
+        //save SQL query as sql
+        const sql = `SELECT CONCAT(first_name," ",last_name) AS Name
+                  FROM employee
+                    LEFT JOIN role ON role.id = employee.role_id
+                    LEFT JOIN department ON department.id = role.department_id
+                    WHERE department_id=?`;
+
+        //mysql query
+        db.query(sql, department, (err, res) => {
+          if (err) {
+            console.log(err);
+          }
+          //construct table in node.js from query
+          console.table(res);
+          //call initializing function to return to inquirer prompt
+          manageEmployees();
+        });
+      });
+  });
+};
